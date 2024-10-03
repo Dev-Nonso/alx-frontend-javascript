7a45eb0d0542d74353775fab17e3a3d294e68abb
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2024-10-03 17:33:13 +0000

    updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = trueOrFalse ? true : false;
-  const task2 = trueOrFalse ? false : true;
+  
+  //var task = false;
+  //var task2 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
 
   return [task, task2];
 }
